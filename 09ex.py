747f76ed6832d211d0496756456696e2e5ea1b0f
Dorayne McKern <dorayne@gmail.com>
2015-10-14 01:35:35 -0700

    Update comments for clarity

--- 09ex.py
+++ 09ex.py
@@ -1,8 +1,10 @@
 # http://learnpythonthehardway.org/book/ex9.html
 
+# initialize variables
 days = "Mon Tue Wed Thu Fri Sat Sun"
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
+# print previous variables
 print "Here are the days: ", days
 print "Here are the months: ", months
 
