d4ce28d2cddd7c423245224ec9fc2f830866151b
Dorayne McKern <dorayne@gmail.com>
2015-10-14 00:46:00 -0700

    Exercise 5, Study drill 3

--- 05ex.py
+++ 05ex.py
@@ -1,6 +1,6 @@
 # http://learnpythonthehardway.org/book/ex5.html
 
-# initialize variables for future user
+# initialize static variables for future arithmetic and printing
 name = 'Dorayne'
 age = 31 # not a lie
 height = 63 # inches
@@ -8,12 +8,16 @@ eyes = 'Blue'
 teeth = 'White'
 hair = 'Lavender'
 
+# initialize relative variable for future printing
+height_cm = height / 0.39370
+
 # print statements using previous variables
 print "Let's talk about %s." % name
 print "She's %d inches tall." % height
+print "Which is %f centimeters tall." % height_cm
 print "She's got %s eyes and %s hair." % (eyes, hair)
 print "Her teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
-print "If I add %d, %d, I get %d." % (
-age, height, age + height)
+print "If I add %d, %d, and %f, I get %f." % (
+age, height, height_cm, age + height + height_cm)
