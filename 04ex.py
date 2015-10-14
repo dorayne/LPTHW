861723e82cd6d18100e964c840c6b34c5f1ce05e
Dorayne McKern <dorayne@gmail.com>
2015-10-14 00:28:49 -0700

    Update comments for clarity

--- 04ex.py
+++ 04ex.py
@@ -1,14 +1,18 @@
 # http://learnpythonthehardway.org/book/ex4.html
 
+# initializing static variables for future arithmetic use and printing
 cars = 100
 space_in_a_car = 4.0
 drivers = 30
 passengers = 90
+
+# initialize relative variables for future printing
 cars_not_driven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
 
+# print info about cars based on previous variable assignment
 print "There are", cars, "cars available."
 print "There are only", drivers, "drivers available."
 print "There will be", cars_not_driven, "empty cars today."
