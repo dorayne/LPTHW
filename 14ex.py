d5d291e636cee35f03992e6ab402a49e469291db
Dorayne McKern <dorayne@gmail.com>
2015-10-14 18:51:40 -0700

    Comments
    Update comments for clarity

--- 14ex.py
+++ 14ex.py
@@ -5,6 +5,7 @@ from sys import argv
 script, user_name = argv
 prompt = '> '
 
+# Get info from the user with a set of questions and prompts
 print "Hi %s, I'm the %s script." % (user_name, script)
 print "I'd like to ask you a few questions."
 print "Do you like me %s?" % user_name
@@ -16,6 +17,7 @@ lives = raw_input(prompt)
 print "What kind of computer do you have?"
 computer = raw_input(prompt)
 
+# Print a summary based on the user input
 print """
 Alright, so you said %r about liking me.
 You live in %r. Not sure where that is.
