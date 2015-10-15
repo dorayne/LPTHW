70a0d6799f9388457df70883ea73419f144710a9
Dorayne McKern <dorayne@gmail.com>
2015-10-15 00:13:40 -0700

    Study Drill 3
    Update to use a single target.write() line instead of 6

--- 16ex.py
+++ 16ex.py
@@ -23,16 +23,12 @@ print "Now I'm going to ask you for three lines."
 line1 = raw_input("line 1: ")
 line2 = raw_input("line 2: ")
 line3 = raw_input("line 3: ")
+new_txt = "%s\n%s\n%s\n" % (line1, line2, line3)
 
 # Write the filer based on user input
 print "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(new_txt)
 
 # Close the file
 print "And finally, we close it."
