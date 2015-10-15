ca3b3212e6625a129c75675558ad2cf663bdd6ff
Dorayne McKern <dorayne@gmail.com>
2015-10-14 18:09:25 -0700

    Update comments for clarity Change data type on last print statement from raw to string, line 9

--- 12ex.py
+++ 12ex.py
@@ -1,8 +1,10 @@
 # http://learnpythonthehardway.org/book/ex12.html
 
+# # playing with putting prompts in raw_input
 age = raw_input("How old are you? ")
 height = raw_input("How tall are you? ")
 weight = raw_input("How much do you weight? ")
 
-print "So, you're %r old, %r tall and %r heavy." % (
+# printing the info acquired via raw_input as strings
+print "So, you're %s old, %s tall and %s heavy." % (
     age, height, weight)
