df74eb40f05ba3ec6f1e23cd282c37888b940c5b
Dorayne McKern <dorayne@gmail.com>
2015-10-14 17:46:14 -0700

    Update comments for clarity

--- 10ex.py
+++ 10ex.py
@@ -1,5 +1,6 @@
 # http://learnpythonthehardway.org/book/ex10.html
 
+# playing with escape sequences
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
