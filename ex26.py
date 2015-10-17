a40f3f373d78aaf556801b07099d04043ba40239
Dorayne McKern <dorayne@gmail.com>
2015-10-16 22:28:27 -0700

    Second round debugging
    Exercise 26

--- ex26.py
+++ ex26.py
@@ -11,7 +11,7 @@ def sort_words(words):
 
 def print_first_word(words):
     """Prints the first word after popping it off."""
-    word = words.poop(0)
+    word = words.pop(0)
     print word
 
 def print_last_word(words):
@@ -45,7 +45,7 @@ poem = """
 with logic so firmly planted
 cannot discern \n the needs of love
 nor comprehend passion from intuition
-and requires an explantion
+and requires an explanation
 \n\t\twhere there is none.
 """
 
@@ -54,7 +54,7 @@ print "--------------"
 print poem
 print "--------------"
 
-five = 10 - 2 + 3 - 5
+five = 10 - 2 + 3 - 6
 print "This should be five: %s" % five
 
 def secret_formula(started):
@@ -68,15 +68,15 @@ start_point = 10000
 beans, jars, crates = secret_formula(start_point)
 
 print "With a starting point of: %d" % start_point
-print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
+print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
 
 start_point = start_point / 10
 
 print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_point)
+print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
 
 
-sentence = "All god\tthings come to those who weight."
+sentence = "All good things come to those who wait."
 
 words = ex25.break_words(sentence)
 sorted_words = ex25.sort_words(words)
@@ -88,6 +88,6 @@ print_last_word(sorted_words)
 sorted_words = ex25.sort_sentence(sentence)
 print sorted_words
 
-print_irst_and_last(sentence)
+print_first_and_last(sentence)
 
-print_first_a_last_sorted(senence)
+print_first_and_last_sorted(sentence)
