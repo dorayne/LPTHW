f6bb62b0574bd574887998ac275d3f2c0dbd92ac
Dorayne McKern <dorayne@gmail.com>
2015-10-16 20:50:13 -0700

    Function Correction
    Correction sort_sentence to match URL

--- ex25.py
+++ ex25.py
@@ -1,3 +1,5 @@
+# http://learnpythonthehardway.org/book/ex25.html
+
 def break_words(stuff):
     """This function will break up words for us."""
     words = stuff.split(' ')
@@ -20,8 +22,7 @@ def print_last_word(words):
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words."""
     words = break_words(sentence)
-    print_first_word(words)
-    print_last_word(words)
+    return sort_words(words)
 
 def print_first_and_last(sentence):
     """Prints the first and last words of the sentence."""
