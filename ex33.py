e7a29394df970f09b1c13de7bd4a4f6078dfe982
Dorayne McKern <dorayne@gmail.com>
2016-06-10 01:55:47 -0700

    Expand while_func
    add user entry for increment interval
    update prompts on raw_input for clarity

--- ex33.py
+++ ex33.py
@@ -1,16 +1,16 @@
 # http://learnpythonthehardway.org/book/ex33.html
 
-def while_func(num):
+def while_func(max_num, inc_num):
     # initialize variables
     i = 0
     number = []
-    while i < num:
+    while i < max_num:
         print "At the top i is %d" % i
         # append i to number list
         number.append(i)
     
         # increment i
-        i = i + 1
+        i = i + inc_num
         print "Numbers now: ", number
         print "At the bottom i is %d\n" % i
 
@@ -20,7 +20,10 @@ def while_func(num):
     for num in number:
         print num
 
-raw_num = raw_input("Enter maximum number: ")
+raw_num = raw_input("What is the maximum number you would like your variable to reach? ")
 int_num = int(raw_num)
 
-while_func(int_num)
+inc_raw = raw_input("By what interval do you want your variable to increment? ")
+inc_num = int(inc_raw)
+
+while_func(int_num, inc_num)
