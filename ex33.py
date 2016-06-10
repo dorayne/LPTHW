e3369e3ff5957e56b057e9e703576fb260860752
Dorayne McKern <dorayne@gmail.com>
2016-06-10 01:50:27 -0700

    Add while_func
    Study drill 1
    create while_func and put previous while-loop into a function
    
    use raw_input to get user entry for a maximum number for i

--- ex33.py
+++ ex33.py
@@ -1,21 +1,26 @@
 # http://learnpythonthehardway.org/book/ex33.html
 
-# initialize variables
-i = 0
-number = []
-
-while i < 6:
-    print "At the top i is %d" % i
-    # append i to number list
-    number.append(i)
+def while_func(num):
+    # initialize variables
+    i = 0
+    number = []
+    while i < num:
+        print "At the top i is %d" % i
+        # append i to number list
+        number.append(i)
     
-    # increment i
-    i = i + 1
-    print "Numbers now: ", number
-    print "At the bottom i is %d\n" % i
+        # increment i
+        i = i + 1
+        print "Numbers now: ", number
+        print "At the bottom i is %d\n" % i
 
-print "The numbers: "
+    print "The numbers: "
 
-# print the contents of number list, 1 per line
-for num in number:
-    print num
+    # print the contents of number list, 1 per line
+    for num in number:
+        print num
+
+raw_num = raw_input("Enter maximum number: ")
+int_num = int(raw_num)
+
+while_func(int_num)
